b5dd8c9ed7a004e25970e0138ccf353800df0e02
chwoznia <chwoznia@cisco.com>
2017-02-16 15:00:49 +0000

    spacing fixes

--- lib/amp4e_ldap_tool/endpoints.rb
+++ lib/amp4e_ldap_tool/endpoints.rb
@@ -5,16 +5,16 @@ module Amp4eLdapTool
                     :group_guid, :policy, :os
 
       def initialize(json)
-        @name = json["hostname"]
-        @guid = json["connector_guid"]
-        @link = { computer: json["links"]["computer"],
-                    trajectory: json["links"]["trajectory"],
-                    groups: json["links"]["group"]}
-        @active = json["active"]
-        @policy = { name: json["policy"]["name"],
-                    guid: json["policy"]["guid"] }
+        @name       = json["hostname"]
+        @guid       = json["connector_guid"]
+        @active     = json["active"]
         @group_guid = json["group_guid"]
-        @os = json["operating_system"]
+        @os         = json["operating_system"]
+        @link       = { computer: json["links"]["computer"],
+                        trajectory: json["links"]["trajectory"],
+                        groups: json["links"]["group"]}
+        @policy     = { name: json["policy"]["name"],
+                        guid: json["policy"]["guid"] }
       end
     end
     
@@ -22,13 +22,13 @@ module Amp4eLdapTool
       attr_accessor :name, :description, :guid, :parent, :link
 
       def initialize(json)
-        @name = json["name"]
-        @guid = json["group_guid"]
-        @description = json["description"]
-        @link = json["links"]["group"]
-        @parent = (json["ancestry"].nil?) ? {} :
-            {name: json["ancestry"].first["name"],
-             guid: json["ancestry"].first["guid"]}
+        @name         = json["name"]
+        @guid         = json["group_guid"]
+        @description  = json["description"]
+        @link         = json["links"]["group"]
+        @parent       = (json["ancestry"].nil?) ? {} :
+                        {name: json["ancestry"].first["name"],
+                         guid: json["ancestry"].first["guid"]}
       end
     end
 
@@ -37,13 +37,13 @@ module Amp4eLdapTool
                     :serial_number, :link
 
       def initialize(json)
-        @name = json["name"]
-        @guid = json["guid"]
-        @description = json["description"]
-        @product = json["product"]
-        @default = json["default"]
+        @name          = json["name"]
+        @guid          = json["guid"]
+        @description   = json["description"]
+        @product       = json["product"]
+        @default       = json["default"]
         @serial_number = json["serial_number"]
-        @link = json["links"]["policy"]
+        @link          = json["links"]["policy"]
       end
     end
   end
