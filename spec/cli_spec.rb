f136cc1a622a78e7a2a0ca3e1e4b2774130a0128
chwoznia <chwoznia@cisco.com>
2017-02-16 15:26:41 +0000

    mocked out AMP:: Endpoints for a cleaner set of tests

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,5 +1,4 @@
 require 'amp4e_ldap_tool/cli'
-require 'amp4e_ldap_tool/endpoints'
 require 'json'
 
 describe Amp4eLdapTool::CLI do
@@ -9,20 +8,12 @@ describe Amp4eLdapTool::CLI do
     context 'amp' do
       let(:amp)       { double("CiscoAMP") }
       let(:output)    { get_output {subject.fetch 'amp'} }
-      let(:computer1) { {hostname: "computer_1", links: {}, policy: {}}.to_json }
-      let(:computer2) { {hostname: "computer_2", links: {}, policy: {}}.to_json }
-      let(:computers) { [Amp4eLdapTool::AMP::Computer.new(computer1),
-                         Amp4eLdapTool::AMP::Computer.new(computer2)] }
-
-      let(:group1)    { {name: "group_1", links: {}, policy: {}}.to_json }
-      let(:group2)    { {name: "group_2", links: {}, policy: {}}.to_json }
-      let(:groups)    { [Amp4eLdapTool::AMP::Group.new(group1),
-                         Amp4eLdapTool::AMP::Group.new(group2)] }
-
-      let(:policy1)   { {name: "policy_1", links: {}}.to_json }
-      let(:policy2)   { {name: "policy_2", links: {}}.to_json }
-      let(:policies)  { [Amp4eLdapTool::AMP::Policy.new(policy1), 
-                         Amp4eLdapTool::AMP::Policy.new(policy2)] }
+      let(:computers) { [double("AMP::Computer", name: "computer_1"), 
+                         double("AMP::Computer", name: "computer_2")] }
+      let(:groups)    { [double("AMP::Group", name: "group_1"), 
+                         double("AMP::Group", name: "group_2")] }
+      let(:policies)  { [double("AMP::Policy", name: "policy_1"), 
+                         double("AMP::Policy", name: "policy_2")] }
 
       before(:each) do
         allow(Amp4eLdapTool::CiscoAMP).to receive(:new).and_return(amp)
