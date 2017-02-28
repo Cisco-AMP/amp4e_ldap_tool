466af3737dfdb7af7b0ed698f89a40112cfaa768
chwoznia <chwoznia@cisco.com>
2017-02-28 03:31:37 +0000

    group output for compairson needs more work

--- lib/amp4e_ldap_tool/cli.rb
+++ lib/amp4e_ldap_tool/cli.rb
@@ -49,26 +49,18 @@ module Amp4eLdapTool
     def view_changes
       amp = Amp4eLdapTool::CiscoAMP.new
       amp_groups = amp.get(:groups)
-      hierarchy = amp_groups.group_by { |g| g.parent }
-      print_hierarchy(hierarchy)
+      amp_groups.each do |x|
+        printf("Group: %-20s Parent: %-20s\n", x.name, x.parent[:name])
+      end
 
       #Get group names, and computer names so we can compare
-      ldap = Amp4eLdapTool::LDAPScrape.new
-      ldiff = ldap.scrape_ldap_entries
+      #ldap = Amp4eLdapTool::LDAPScrape.new
+      #ldiff = ldap.scrape_ldap_entries
 
-      Amp4eLdapTool.tree(amp, ldiff)
+      #Amp4eLdapTool.tree(amp, ldiff)
     end
 
     private
-    def print_hierarchy(hier)
-      hier.keys.each do |parent|
-        puts "root" if (parent.empty?)
-        puts parent["name"] unless (parent == {})
-        hier[parent].each do |child|
-          puts "\t#{child.name}"
-        end
-      end
-    end
 
     def display_resources(amp, options)
       options.keys.each do |endpoints|
